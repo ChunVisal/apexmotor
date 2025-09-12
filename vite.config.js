aa1314c339ac76b46b6dfd0d4190438a1bd972db
Joe Narthan <JoeNarthan@users.noreply.github.com>
2025-09-12 16:29:14 +0700

    save before version bump

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [ react(), tailwindcss()],
-  base: '/apexmotor/',
+  base: '/.',
     build: {
     outDir: 'dist',
-  },
+  }, 
+  chunkSizeWarningLimit: 500,
 })
 
